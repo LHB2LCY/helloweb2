a0b97e25be8fd78af04386c44184c6767c5248f3
liangshuang <297089375@qq.com>
2019-05-20 00:38:23 +0800

    idea  提交

--- src/com/lhb/Main.java
+++ src/com/lhb/Main.java
@@ -6,13 +6,13 @@ public class Main {
 
 
         System.out.println("上传代码");
-        
+
 
         getName();
     }
 
     private static void getName() {
 
-
+        System.out.println("sdjofjsd");
     }
 }
--- src/com/lhb/UserDao.java
+++ src/com/lhb/UserDao.java
@@ -3,5 +3,6 @@ package com.lhb;
 public interface UserDao {
 //解释
     void  find();
+    void save();
 
 }
